d6d4f929414e7d6e012a2ab380f0db067e62f1ef
jayden-lee <gwanggi.lee@gmail.com>
2020-01-20 18:08:39 +0900

    예제 도메인 모델 추가

---
+++ src/test/java/com/jayden/study/querydsl/entity/MemberTest.java
@@ -0,0 +1,46 @@
+package com.jayden.study.querydsl.entity;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+class MemberTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void testEntity() {
+        Team teamA = new Team("Team A");
+        Team teamB = new Team("Team B");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        // flush
+        em.flush();
+        em.clear();
+
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+            .getResultList();
+
+        Assertions.assertEquals(4, members.size());
+    }
+}
