2e389b45168b723018f4936734f0fe3cc4a97d1f
jayden-lee <gwanggi.lee@gmail.com>
2020-01-31 16:44:12 +0900

    순수 JPA와 Querydsl

---
+++ src/main/java/com/jayden/study/querydsl/init/InitMember.java
@@ -0,0 +1,44 @@
+package com.jayden.study.querydsl.init;
+
+import com.jayden.study.querydsl.entity.Member;
+import com.jayden.study.querydsl.entity.Team;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Profile("local")
+@Component
+@RequiredArgsConstructor
+public class InitMember {
+
+    private final InitMemberService initMemberService;
+
+    @PostConstruct
+    public void init() {
+        initMemberService.init();
+    }
+
+    @Component
+    static class InitMemberService {
+        @PersistenceContext
+        private EntityManager em;
+
+        @Transactional
+        public void init() {
+            Team teamA = new Team("Team A");
+            Team teamB = new Team("Team B");
+            em.persist(teamA);
+            em.persist(teamB);
+
+            for (int i = 0; i < 100; i++) {
+                Team selectedTeam = i % 2 == 0 ? teamA : teamB;
+                em.persist(new Member("member" + i, i, selectedTeam));
+            }
+        }
+    }
+}
